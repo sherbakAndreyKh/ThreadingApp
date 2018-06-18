2d05374a124b41653bbd63ddc17310eadf8ffeb1
sherbakAndreyKh <SherbakAndreyKh@gmail.com>
2018-06-18 14:55:29 +0300

    try

--- ThreadingApp/ThreadingApp/EaterModel.cs
+++ ThreadingApp/ThreadingApp/EaterModel.cs
@@ -8,7 +8,11 @@ namespace ThreadingApp
 {
    public class EaterModel
     {
+
         public string Name { get; set; }
+        public int Position { get; set; }
+        public bool Cutlery { get; set; }
+        static object locker = new object();
 
         public EaterModel()
         {
@@ -20,5 +24,33 @@ namespace ThreadingApp
             this.Name = Name;
         }
 
+       public void Eat(List<EaterModel> eaters)
+        {
+            lock (locker)
+            {
+                if (Position == 0)
+                {
+                    if (Cutlery == false && eaters[eaters.Count - 1].Cutlery == false)
+                    {
+                        Cutlery = true;
+                        eaters[eaters.Count - 1].Cutlery = true;
+                        Console.WriteLine(Name + " Eating");
+                        Console.WriteLine();
+                        Cutlery = false;
+                        eaters[eaters.Count - 1].Cutlery = false;
+                    }
+                   
+                }
+                else if (Cutlery == false && eaters[Position - 1].Cutlery == false)
+                {
+                    Cutlery = true;
+                    eaters[Position - 1].Cutlery = true;
+                    Console.WriteLine(Name + " Eating");
+                    Console.WriteLine();
+                    Cutlery = false;
+                    eaters[Position - 1].Cutlery = false;
+                }
+            }
+        }
     }
 }
